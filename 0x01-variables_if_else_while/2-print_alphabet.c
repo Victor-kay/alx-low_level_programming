c2a7252c23e6ff8a9e60dd9625b3cb87e22b01e8
victor-kay <jvictorkingorikay94@gmail.com>
2023-02-17 18:15:28 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - Prints the alphabetic
@@ -11,7 +11,7 @@ int main(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 	putchar('\n');
 	return (0);
