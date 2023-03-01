8e5751c1611cd815702f05af3588736e2ae8b6f6
victor-kay <jvictorkingorikay94@gmail.com>
2023-02-28 17:59:01 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,15 @@
 
 /**
  * _puts - Prints a string
- * @str: The string to print
- * Return: void
+ * @str: string to be printed
  */
 
 void _puts(char *str)
 
 {
-	for (; *str != '\0'; str++)
+	while (*str != '\0')
 	{
-	_putchar(*str);
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
