b72f508d82f535fa4042a17f7078eb4e8b3e8b88
victor-kay <jvictorkingorikay94@gmail.com>
2023-02-28 18:59:46 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * * main - check the code
+ *  *
+ *    * Return: Always 0.
+ *      */
+int main(void)
+{
+	 char *str;
+	 	
+	 	 char *str;
+		 puts_half(str);
+		 return (0);
+}
